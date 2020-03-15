236d9fc23c0f1da794341ee431b4021f2f228097
whoiseve <iamevemusic@gmail.com>
2020-03-15 05:05:35 -0700

    .1 to.2 + scalar 10 - 9

--- parallax.js
+++ parallax.js
@@ -25,12 +25,12 @@
     invertY: true,
     limitX: false,
     limitY: false,
-    scalarX: 10.0,
-    scalarY: 10.0,
+    scalarX: 9.0,
+    scalarY: 9.0,
     frictionX: 0.1,
     frictionY: 0.1,
-    originX: 0.1,
-    originY: 0.1,
+    originX: 0.2,
+    originY: 0.2,
     pointerEvents: true,
     precision: 1
   };
