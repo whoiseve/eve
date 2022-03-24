e183bdf79c6feab49691abe44abd3fd8648bd5ef
Geordie <mgmt@creator.contact>
2022-03-24 00:30:18 -0800

    Added secondary help box & Konami Code

--- script.js
+++ script.js
@@ -1,3 +1,84 @@
+/**
+ * Handles KeySequences
+ * @property {KeySequence[]} keySequences - An array of key sequences.
+ * @property {number[]} keys - An array of keys to process on keydown.
+ */
+ function KeySequenceHandler (keySequences) {
+  this.keySequences = keySequences;
+  this.keys = [];
+
+  this.init();
+}
+
+/**
+* Populates keys array and readies keydown event listener.
+*/
+KeySequenceHandler.prototype.init = function () {
+  this.keys = this.getKeys(this.keySequences);
+
+  document.addEventListener('keydown', function(e) {
+      var keyCode = e.keyCode;
+      if (this.keys.indexOf(keyCode) != -1) {
+          for (var i = 0; i < this.keySequences.length; i++) {
+              this.keySequences[i].step(keyCode);
+          }
+      }
+  }.bind(this));
+};
+
+/**
+* Get all keys from key sequences
+* @param {KeySequence[]} keySequences - An array of key sequences.
+* @return {number[]} - A deduped array of keys used in all key sequences.
+*/
+KeySequenceHandler.prototype.getKeys = function (keySequences) {
+  var keys = [];
+
+  // for each key sequence
+  for (var i = 0; i < keySequences.length; i++) {
+
+      // for each key in sequence
+      var sequence = keySequences[i].sequence;
+      for (var j = 0; j < sequence.length; j++) {
+
+          var key = sequence[j];
+          if (keys.indexOf(key) == -1) {
+              keys.push(key);
+          }
+      }
+  }
+  return keys;
+};
+
+/**
+* Defines a sequence of keypresses with accompanying code to execute.
+* @param {number[]} sequence - A sequence of keys.
+* @param {Function} callback - A function to be called when the sequence is completed.
+* @property {number} position - A number for tracking position in the sequence.
+*/
+function KeySequence (sequence, callback) {
+  this.sequence = sequence;
+  this.callback = (callback === undefined) ? Function.prototype : callback; // noop?
+  this.position = 0;
+}
+
+/**
+* Steps through the sequence.
+* @param {number} keyCode - A key code to check.
+*/
+KeySequence.prototype.step = function (keyCode) {
+  var req = this.sequence[this.position];
+  if (keyCode == req) {
+      this.position++;
+      if (this.position == this.sequence.length) {
+          this.position = 0;
+          this.callback(); // hurrah!
+      }
+  } else {
+      this.position = 0;
+  }
+};
+
 var width, height, container, canvas, ctx, points, target, animateHeader = true;
 
 function init() {
@@ -195,151 +276,159 @@ init();
 
 
 ;(function(window) {
-
 	'use strict';
 
-		//FIND IP
+  // example sequences
+var sequence1 = new KeySequence(
+  [38, 38, 40, 40, 37, 39,37 ,39, 66, 65],
+  function () { 
+    // DO THINGSSSSSS
+    alert('hello, world.');
+  }
+);
 
-		function findIP(onNewIP) { //  onNewIp - your listener function for new IPs
-		  var myPeerConnection = window.RTCPeerConnection || window.mozRTCPeerConnection || window.webkitRTCPeerConnection; //compatibility for firefox and chrome
-		  var pc = new myPeerConnection({iceServers: []}),
-		    noop = function() {},
-		    localIPs = {},
-		    ipRegex = /([0-9]{1,3}(\.[0-9]{1,3}){3}|[a-f0-9]{1,4}(:[a-f0-9]{1,4}){7})/g,
-		    key;
+  // example sequences
+  var sequence2 = new KeySequence(
+    [67, 79, 67, 75],
+    function () { 
+      // DO THINGSSSSSS
+      alert('Hey..... NICE COCKhhhhh');
+    }
+  );
 
-		  function ipIterate(ip) {
-		    if (!localIPs[ip]) onNewIP(ip);
-		    localIPs[ip] = true;
-		  }
-		  pc.createDataChannel(""); //create a bogus data channel
-		  pc.createOffer(function(sdp) {
-		    sdp.sdp.split('\n').forEach(function(line) {
-		      if (line.indexOf('candidate') < 0) return;
-		      line.match(ipRegex).forEach(ipIterate);
-		    });
-		    pc.setLocalDescription(sdp, noop, noop);
-		  }, noop); // create offer and set local description
-		  pc.onicecandidate = function(ice) { //listen for candidate events
-		    if (!ice || !ice.candidate || !ice.candidate.candidate || !ice.candidate.candidate.match(ipRegex)) return;
-		    ice.candidate.candidate.match(ipRegex).forEach(ipIterate);
-		  };
-		}
+// init
+new KeySequenceHandler([sequence1,sequence2]);
 
-function addIP(ip) {
-  console.log('got ip: ', ip);
+  // FIND IP
 
-	var theIp = document.getElementById('ip');
-	var theConsole = $('span.console');
-	var texted = ip;
+  function findIP(onNewIP) { //  onNewIp - your listener function for new IPs
+    var myPeerConnection = window.RTCPeerConnection || window.mozRTCPeerConnection || window.webkitRTCPeerConnection; //compatibility for firefox and chrome
+    var pc = new myPeerConnection({iceServers: []}),
+      noop = function() {},
+      localIPs = {},
+      ipRegex = /([0-9]{1,3}(\.[0-9]{1,3}){3}|[a-f0-9]{1,4}(:[a-f0-9]{1,4}){7})/g,
+      key;
 
-  theIp.textContent = ip;
+    function ipIterate(ip) {
+      if (!localIPs[ip]) onNewIP(ip);
+      localIPs[ip] = true;
+    }
+    pc.createDataChannel(""); //create a bogus data channel
+    pc.createOffer(function(sdp) {
+      sdp.sdp.split('\n').forEach(function(line) {
+        if (line.indexOf('candidate') < 0) return;
+        line.match(ipRegex).forEach(ipIterate);
+      });
+      pc.setLocalDescription(sdp, noop, noop);
+    }, noop); // create offer and set local description
+    pc.onicecandidate = function(ice) { //listen for candidate events
+      if (!ice || !ice.candidate || !ice.candidate.candidate || !ice.candidate.candidate.match(ipRegex)) return;
+      ice.candidate.candidate.match(ipRegex).forEach(ipIterate);
+    };
+  }
 
+  function addIP(ip) {
+    console.log('got ip: ', ip);
 
+    var theIp = document.getElementById('ip');
+    var theConsole = $('span.console');
+    var texted = ip;
 
-	theConsole.html(texted);
+    theIp.textContent = ip;
+    theConsole.html(texted);
+  }
 
-}
+  findIP(addIP);
 
-findIP(addIP);
+  // FIND LOCATION
 
-//FIND LOCATIOn
-
-
-$.getJSON('https://ipapi.co/'+$(ip).val()+'/json', function(data){
-
-      $('.country').text(data.country);
+  $.getJSON('https://ipapi.co/'+$(ip).val()+'/json', function(data) {
+    $('.country').text(data.country);
   });
 
-
 	(function() {
-
 		var theConsole = $('span.console');
 		var texted = $("#ip").text();
-
 		theConsole.html(texted);
 	});
 
-var search_form = document.getElementsByClassName('search__form');
-console.log(search_form);
+  var search_form = document.getElementsByClassName('search__form');
+  console.log(search_form);
 
+  function createHome() {
+    var homeDiv = document.createElement('div');
+    homeDiv.innerHTML = '<div class="home_container"><h2>HELP INFORMATION:</h2><p>Please try inputting a command, and pressing "enter". <br/> You can see the list of commands under "Navigation Portals".</p><div class="close_home" href="">x</div></div>';
+    homeDiv.setAttribute('class', 'home');
+    document.body.appendChild(homeDiv);
 
-
-function createHome(){
-
-  var homeDiv = document.createElement('div');
-        homeDiv.innerHTML = '<div class="home_container"><h2>HELP INFORMATION:</h2><p>Please try inputting a command, and pressing "enter". <br/> You can see the list of commands under "Navigation Portals".</p><div class="close_home" href="">x</div></div>';
-        homeDiv.setAttribute('class', 'home');
-        document.body.appendChild(homeDiv);
-
-        $('.close_home').click(function(){
-            $('.home').remove();
-            console.log('Home Erased');
-        });
-
-}
-
-var navigationLink = $('.terminal__line a');
-
-navigationLink.click(function(e){
-  if ($(this).hasClass('out')) {
-    window.open('http://instagram.com/');
-  }else
-  {
-  createHome();
+    $('.close_home').click(function() {
+      $('.home').remove();
+      console.log('Home Erased');
+    });
   }
-});
 
+  function createAss() {
+    var homeDiv = document.createElement('div');
+    homeDiv.innerHTML = '<div class="home_container"><h2>ASSSSSSSSS:</h2><p>Please try inputting a command, and pressing "enter". <br/> You can see the list of commands under "Navigation Portals".</p><div class="close_home" href="">x</div></div>';
+    homeDiv.setAttribute('class', 'home');
+    document.body.appendChild(homeDiv);
 
+    $('.close_home').click(function() {
+      $('.home').remove();
+      console.log('Home Erased');
+    });
+  }
 
-	$(search_form).submit(function( event ) {
-	  if ( 'help' === $( "input" ).val() || 'ass' === $( "input" ).val() ||  'keyword2' === $( "input" ).val() || 'keyword3' === $( "input" ).val() || 'keyword4' === $( "input" ).val() || 'keyword5'  === $( "input" ).val()) {
+  var navigationLink = $('.terminal__line a');
+  navigationLink.click(function(e) {
+    if ($(this).hasClass('out')) {
+      window.open('http://instagram.com/');
+    } else {
+      createHome();
+    }
+  });
 
-    createHome();
+  $(search_form).submit(function( event ) {
+    if ('ass' === $( "input" ).val()) {
+      createAss();
+    } else if ( 'help' === $( "input" ).val() || 'keyword2' === $( "input" ).val() || 'keyword3' === $( "input" ).val() || 'keyword4' === $( "input" ).val() || 'keyword5'  === $( "input" ).val()) {
+      createHome();
+    } else if ( $( "input" ).val() === "about" ) {
+      window.open('http://eveassistant.com/home',"_self");
+    } else if ( $( "input" ).val() === "music" ) {
+      window.open('http://eveassistant.com/music',"_self");
+    } else if ( $( "input" ).val() === "console" ) {
+      window.open('http://www.eveassistant.com/interact',"_self");
+    } else if ( $( "input" ).val() === "social" ) {
+      window.open('http://www.jemi.app/iameve',"_self");
+    } else if ( $( "input" ).val() === "discord" ) {
+      window.open('https://discord.gg/Y5p7cfvYTs',"_self");
+    } else if ( $( "input" ).val() === "admin" ) {
+      window.open('http://www.eveassistant.com/admin',"_self");
 
-	  } else if ( $( "input" ).val() === "about" ) {
-				window.open('http://eveassistant.com/home',"_self");
-      } else if ( $( "input" ).val() === "music" ) {
-				window.open('http://eveassistant.com/music',"_self");
-      } else if ( $( "input" ).val() === "console" ) {
-				window.open('http://www.eveassistant.com/interact',"_self");
-      } else if ( $( "input" ).val() === "social" ) {
-				window.open('http://www.jemi.app/iameve',"_self");
-      } else if ( $( "input" ).val() === "discord" ) {
-				window.open('https://discord.gg/Y5p7cfvYTs',"_self");
-      } else if ( $( "input" ).val() === "admin" ) {
-				window.open('http://www.eveassistant.com/admin',"_self");
+      var binder = $('input').val();
+      var terminal_div = document.getElementsByClassName('terminal');
+      $('.terminal').addClass("binding");
+      var theipagain = $('#ip').html();
 
-        var binder = $('input').val();
-        var terminal_div = document.getElementsByClassName('terminal');
-            $('.terminal').addClass("binding");
-        var theipagain = $('#ip').html();
+      var ipconfig = document.createElement('p');
+      $(ipconfig).text('ipconfig: ' + theipagain);
+      ipconfig.setAttribute('class', 'terminal__line');
+      $(ipconfig).appendTo(terminal_div);
+      console.log(ipconfig.length);
 
-        var ipconfig = document.createElement('p');
-              $(ipconfig).text('ipconfig: ' + theipagain);
-              ipconfig.setAttribute('class', 'terminal__line');
-              $(ipconfig).appendTo(terminal_div);
-              console.log(ipconfig.length);
+    }
 
-      }
-      
+    var binder = $('input').val();
+    var terminal_div = document.getElementsByClassName('terminal');
+    $('.terminal').addClass("binding");
 
-		var binder = $('input').val();
-		var terminal_div = document.getElementsByClassName('terminal');
-				$('.terminal').addClass("binding");
+    var commands = document.createElement('p');
+    commands.innerHTML = ('Execute: ' + binder);
+    commands.setAttribute('class', 'terminal__line');
+    $(commands).appendTo(terminal_div);
 
-		var commands = document.createElement('p');
-					commands.innerHTML = ('Execute: ' + binder);
-					commands.setAttribute('class', 'terminal__line');
-					$(commands).appendTo(terminal_div);
-
-
-
-
-
-	  event.preventDefault();
-});
-
-
+    event.preventDefault();
+  });
 
 })(window);
